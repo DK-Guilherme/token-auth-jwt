05c36fdd39c10070164d42aed3837e61370888ff
DK-Guilherme <guilhermegsouza520@gmail.com>
2022-07-07 15:16:27 -0300

    initial commit

---
+++ token-auth-jwt/src/main/kotlin/com/dkcommunity/tokenauthjwt/TokenAuthJwtApplication.kt
@@ -0,0 +1,11 @@
+package com.dkcommunity.tokenauthjwt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TokenAuthJwtApplication
+
+fun main(args: Array<String>) {
+	runApplication<TokenAuthJwtApplication>(*args)
+}
---
+++ token-auth-jwt/src/test/kotlin/com/dkcommunity/tokenauthjwt/TokenAuthJwtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dkcommunity.tokenauthjwt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TokenAuthJwtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
